c02f40f410bc682c9e06d919ad673b4ff95f9974
Arnoud <varnoud67@gmail.com>
2025-02-22 16:04:25 +0100

    test and bin

--- Computer_Sim/src/domein/BinaryFunctions.java
+++ Computer_Sim/src/domein/BinaryFunctions.java
@@ -1,10 +1,13 @@
 package domein;
 
 public class BinaryFunctions {
+
 	public int binToDec(String binNum) {
 		int decNum = 0;
-		for (int i = 0; i < 7; i++) {
-			decNum += binNum.charAt(i) * Math.pow(2, i);
+		for (int i = 0; i < 8; i++) {
+			if (binNum.charAt(8 - 1 - i) == '1') {
+				decNum += Math.pow(2, i);
+			}
 		}
 		return decNum;
 	}
--- Computer_Sim/src/testen/BinaryFunctionsTest.java
+++ Computer_Sim/src/testen/BinaryFunctionsTest.java
@@ -1,14 +1,21 @@
 package testen;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+
+import domein.BinaryFunctions;
 
 class BinaryFunctionsTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	void testBinToDec() {
+		BinaryFunctions binaryFunctions = new BinaryFunctions();
 
+		assertEquals(10, binaryFunctions.binToDec("00001010"));
+		assertEquals(7, binaryFunctions.binToDec("00000111"));
+		assertEquals(94, binaryFunctions.binToDec("01011110"));
+		assertEquals(0, binaryFunctions.binToDec("00000000"));
+		assertEquals(255, binaryFunctions.binToDec("11111111"));
+	}
 }
