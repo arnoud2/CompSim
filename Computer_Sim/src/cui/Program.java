d194a41bf61a8e518b4353d316fd839669ad6587
Arnoud <varnoud67@gmail.com>
2025-02-23 19:09:34 +0100

    Mult test

--- Computer_Sim/src/cui/Program.java
+++ Computer_Sim/src/cui/Program.java
@@ -13,9 +13,13 @@ public class Program {
     public void startUp() {
         int[] code = 
         	{
-        	14, 1, 0, 10,
-        	14, 2, 0, 5,
-        	1, 1, 2, 3
+        	14, 1, 0, 21,  //reg1 = 6
+        	14, 2, 0, 9,   //reg2 = 5
+        	14, 13, 0, 1,  //reg13 = con 1
+        	14, 14, 0, 5,  //regjump = 5
+        	1, 3, 1, 3 ,   //reg3 += reg1
+        	1, 12, 13, 12, //reg12 = counter++
+        	9, 12, 2, 0    //reg12 != reg2 then jump
         	};
         
         int pc = 0;
